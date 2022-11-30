3f36700ba037e8b0205862948ab43aed4ed78747
Saniul <saniulmallik@techis.io>
2022-11-30 14:24:58 +0530

    trusted origin added for replit

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 X_FRAME_OPTIONS = '*'
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ['https://127.0.0.1:8000']
+CSRF_TRUSTED_ORIGINS = ['https://127.0.0.1:8000', 'http://0.0.0.0:3000/',
+                        'https://djangoforum.saniulmallik.repl.co/', ]
 
 
 # Application definition
